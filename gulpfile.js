4399c6f58c9db496decd8fed4e157d995fdec126
Santiago Serna <sanserve91@gmail.com>
2016-03-23 21:09:40 -0500

    se añaden archivos que estaban haciendo falta

--- gulpfile.js
+++ gulpfile.js
@@ -201,7 +201,7 @@ gulp.task('build:html', function(){
     .pipe(gulpif('*.html', htmlmin({
       removeComments: true,
       collapseWhitespace: true,
-      removeEmptyElements: true,
+      removeEmptyElements: false,
       keepClosingSlash: true
     })))
     // Output files
